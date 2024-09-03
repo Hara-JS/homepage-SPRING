f2655c9d51184d79cfca155ef39428d4a7a59742
haralee <jisunhara@gmail.com>
2024-09-03 18:45:52 +0900

    SPRING으로 홈페이지 구현
    2024-09-03 hara

---
+++ pinocchio/pinocchio/src/test/java/com/geppetto/hara/pinocchio/PinocchioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geppetto.hara.pinocchio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinocchioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
